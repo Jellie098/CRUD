dd22db68280ce17b5c7b05eb61b10337c5ade460
Leslie Esparza <leslie.esparzamz@gmail.com>
2020-11-04 00:06:32 -0600

    Agregar rutas al archivo web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Listado de productos
+Route::get('/productos', [ProductosController::class, 'index']);
+Route::get('/productos/create', [ProductosController::class, 'create']);
