8ccfafa11ec1e0079a5fd8ee64b0ba6e9f0a22fd
Leslie Esparza <leslie.esparzamz@gmail.com>
2020-11-23 21:13:40 -0600

    Separación en distintas vistas

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,6 +2,17 @@
 
 @section('contenido')
 
-<h2>Contenido de página</h2>
+<div class="wrapper wrapper-content animated fadeInRight">
+        <div class="ibox float-e-margins">
+            <div class="ibox-title">
+                <div class="middle-box text-center animated fadeInRightBig">
+                    <h2>Bienvenido al sistema del vivero</h2>
+                    <br><br><br><br><br><br><br><br><br>
+                </div>    
+                
+            </div>    
+
+        </div>
+</div>
 
 @endsection
---
+++ resources/views/layouts/menuLateral.blade.php
@@ -0,0 +1,26 @@
+<ul class="nav metismenu" id="side-menu">
+    <li class="nav-header">
+        <div class="dropdown profile-element"> <span>
+            <img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}" />
+            </span>
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">CactiPlace</strong>
+                </span> <span class="text-muted text-xs block">Bienvenido <b class="caret"></b></span> </span> </a>
+            <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                <li><a href="profile.html">Perfil</a></li>
+                <li class="divider"></li>
+                <li><a href="login.html">Logout</a></li>
+            </ul>
+        </div>
+        <div class="logo-element">
+            C
+        </div>
+    </li>
+    <li>
+        <a href="layouts.html"><i class="fa fa-shopping-cart"></i> <span class="nav-label">Hacer pedido</span></a>
+    </li>
+    <li>
+        <a href="metrics.html"><i class="fa fa-edit"></i> <span class="nav-label">Pedidos</span>  </a>
+    </li>
+
+</ul>
